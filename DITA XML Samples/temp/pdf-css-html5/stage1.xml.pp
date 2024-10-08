9dc4cc4ad8e844701b7422d1a79e081d95e27a25
PriyaTechWriter <priyadeshmukh693@gmail.com>
2024-10-08 14:55:18 +0530

    Add files via upload

---
+++ DITA XML Samples/temp/pdf-css-html5/stage1.xml.pp
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?><map xmlns:css2fo="http://www.oxygenxml.com/css2fo" xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" xmlns:ditaarch="http://dita.oasis-open.org/architecture/2005/" xmlns:nd="http://www.oxygenxml.com/css2fo/named-destinations" xmlns:opentopic="http://www.idiominc.com/opentopic" xmlns:opentopic-index="http://www.idiominc.com/opentopic/index" xmlns:oxy="http://www.oxygenxml.com/extensions/author" xmlns:xs="http://www.w3.org/2001/XMLSchema" cascade="merge" class="- map/map " ditaarch:DITAArchVersion="1.3" domains="(map mapgroup-d) (topic abbrev-d) (topic delay-d) a(props deliveryTarget) (map ditavalref-d) (map glossref-d) (topic hazard-d) (topic hi-d) (topic indexing-d) (topic markup-d) (topic pr-d) (topic relmgmt-d) (topic sw-d) (topic ui-d) (topic ut-d) (topic markup-d xml-d)" specializations="@props/deliveryTarget" xtrc="map:1;3:6" xtrf="file:/C:/Users/DELL/Desktop/DITA%20XML%20Samples/end_user_guide_for_bmi_calculator_app.ditamap" xml:lang="en"><oxy:front-page class="- front-page/front-page "><oxy:front-page-title class="- front-page/front-page-title "><title xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- topic/title ">End-User Guide for BMI Calculator App</title></oxy:front-page-title></oxy:front-page><opentopic:map xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- toc/toc "><oxy:toc-title class="- toc/title " empty="true"/>
+    <title class="- topic/title ">End-User Guide for BMI Calculator App</title>
+    <topichead is-chapter="true" class="+ map/topicref mapgroup-d/topichead " navtitle="BMI"><topicmeta class="- map/topicmeta "><navtitle class="- topic/navtitle " href="#d81e14">BMI</navtitle></topicmeta><topicref class="- map/topicref " href="#unique_1" type="task"><topicmeta class="- map/topicmeta " data-topic-id="how_to_calculate_bmi"><resourceid appid="how_to_calculate_bmi" class="- topic/resourceid " oxy-source="topic"/><navtitle href="#unique_1" class="- topic/navtitle ">How to Calculate BMI ?</navtitle><?ditaot gentext?><?ditaot genshortdesc?><shortdesc class="- map/shortdesc ">Here are the instructions for using BMI calculator app</shortdesc></topicmeta></topicref><topicref class="- map/topicref " href="#unique_2" type="concept"><topicmeta class="- map/topicmeta " data-topic-id="what_is_bmi"><resourceid appid="what_is_bmi" class="- topic/resourceid " oxy-source="topic"/><navtitle href="#unique_2" class="- topic/navtitle ">What is BMI ?</navtitle><?ditaot gentext?><?ditaot genshortdesc?><shortdesc class="- map/shortdesc ">Body Mass Index (BMI) is a simple and widely used method to estimate an individual's         body fat based on their weight with respect to their height. It’s commonly used by         healthcare professionals to evaluate the risk of developing health conditions related to         body weight, such as obesity, diabetes, hypertension, cardiovascular diseases, and certain         types of cancer. It provides numerical values that are catergorized into different ranges,         indicating whether they are underweight,normal weight, overweight, or obese.</shortdesc></topicmeta></topicref></topichead>
+</opentopic:map><topic xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" break-before="true" is-chapter="true" class="+ topic/topic pdf2-d/placeholder " id="d81e14" topicrefclass="+ map/topicref mapgroup-d/topichead "><title class="- topic/title "><ph class="- topic/ph topic/title-wrapper ">BMI</ph></title><task class="- topic/topic task/task " id="unique_1" topicrefclass="- map/topicref " nd:nd-id="how_to_calculate_bmi" oid="how_to_calculate_bmi" specializations="@props/deliveryTarget">
+    <title class="- topic/title "><ph class="- topic/ph topic/title-wrapper ">How to Calculate BMI ?</ph></title>
+    <shortdesc class="- topic/shortdesc ">Here are the instructions for using BMI calculator app</shortdesc>
+    <taskbody class="- topic/body task/taskbody ">
+        <prereq class="- topic/section task/prereq " id="unique_1_Connect_42_prereq_t1j_wjd_zcc" nd:nd-id="prereq_t1j_wjd_zcc">You must know the measurement of your body weight and
+            height.</prereq>
+        <steps class="- topic/ol task/steps ">
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Enter Gender (Male/Female)</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Enter Age</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Select the unit of measurement for weight (kg/lbs).</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Enter weight.</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Select the unit of measurement for height (ft/inch/cm/m).</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Enter height.</cmd>
+            </step>
+            <step class="- topic/li task/step ">
+                <cmd class="- topic/ph task/cmd ">Tap Submit button.</cmd>
+            </step>
+        </steps>
+        <result class="- topic/section task/result " id="unique_1_Connect_42_result_hnk_3sd_zcc" nd:nd-id="result_hnk_3sd_zcc">The result from BMI calculation is displayed along with the
+            corresponding weight category.</result>
+        <example class="- topic/example " id="unique_1_Connect_42_example_ijv_msd_zcc" nd:nd-id="example_ijv_msd_zcc">
+            <p class="- topic/p ">You want to find your BMI for a woman aged 26 years. Enter Gender as Female. Enter
+                Age 26 years. Select unit of measurement for weight as kg. Enter weight as 65
+                kg.</p>
+            <p class="- topic/p ">Select unit of measurement for height as cm. Enter weight as 161 cm. The result is
+                displayed as 24.2, Normal Weight.</p>
+        </example>
+    </taskbody>
+</task><concept class="- topic/topic concept/concept " id="unique_2" topicrefclass="- map/topicref " nd:nd-id="what_is_bmi" oid="what_is_bmi" specializations="@props/deliveryTarget">
+    <title class="- topic/title "><ph class="- topic/ph topic/title-wrapper ">What is BMI ?</ph></title>
+    <shortdesc class="- topic/shortdesc ">Body Mass Index (BMI) is a simple and widely used method to estimate an individual's
+        body fat based on their weight with respect to their height. It’s commonly used by
+        healthcare professionals to evaluate the risk of developing health conditions related to
+        body weight, such as obesity, diabetes, hypertension, cardiovascular diseases, and certain
+        types of cancer. It provides numerical values that are catergorized into different ranges,
+        indicating whether they are underweight,normal weight, overweight, or obese.</shortdesc>
+    <conbody class="- topic/body  concept/conbody ">
+        <ul class="- topic/ul " id="unique_2_Connect_42_ul_dwy_2wd_zcc" nd:nd-id="ul_dwy_2wd_zcc">
+            <li class="- topic/li ">Underweight: BMI &lt; 18.5</li>
+            <li class="- topic/li ">Normal Weight: BMI 18.5–24.9</li>
+            <li class="- topic/li ">Overweight: BMI 25–29.9</li>
+            <li class="- topic/li ">Obese: BMI &gt;= 30</li>
+        </ul>
+        <p class="- topic/p "><b class="+ topic/ph hi-d/b ">Formula to calculate BMI:</b></p>
+        <p class="- topic/p ">BMI (standard units) = Weight (kg)/Height (m)sq</p>
+        <p class="- topic/p ">BMI (imperial units) = Weight (lbs)*703/Height (in)sq</p>
+    </conbody>
+</concept></topic></map>
